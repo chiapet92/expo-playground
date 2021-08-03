af0d70f51fa131bb218660d57a2b3b4f6809c066
chiapet92 <markxqi@gmail.com>
2021-08-02 21:19:11 -0700

    flex size

--- App.js
+++ App.js
@@ -1,43 +1,18 @@
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Platform,
-} from "react-native";
-// preferred way to detect screen flips
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 // View -> UIView for iOS
 export default function App() {
-  console.log("App executed");
-  console.log(Dimensions.get("screen"));
-  console.log(useDimensions());
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "50%",
-          // width: 150, // density independent pixels, also can do "50%"
-          // height: 70,
-        }}
-      ></View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+      }}
+    >
+      <View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
+      <View style={{ backgroundColor: "gold", flex: 2 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? 20 : 0,
-  },
-});
