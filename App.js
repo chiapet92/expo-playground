86a18527a4722581876e77b2b56009a03623b31f
chiapet92 <markxqi@gmail.com>
2021-08-02 21:26:01 -0700

    more alignment junk

--- App.js
+++ App.js
@@ -11,13 +11,19 @@ export default function App() {
         flexDirection: "row", // horizontal
         justifyContent: "center", // main
         alignItems: "center", // secondary axis
+        alignContent: "center", // only applies with wrapping
+        flexWrap: "wrap",
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
+      <View
+        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
+      />
     </View>
   );
 }
