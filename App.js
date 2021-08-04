e6a279493eb984b82d673fb0422aed082446a3ec
chiapet92 <markxqi@gmail.com>
2021-08-03 23:22:09 -0700

    building the card component

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Card from "./app/components/Card";
 
 // View -> UIView for iOS
 export default function App() {
   return (
     <View
       style={{
-        flex: 1,
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
       }}
     >
-      <WelcomeScreen />
+      <Card
+        title="Red jacket for sale"
+        subtitle="$100"
+        image={require("./app/assets/chair.jpg")}
+      />
     </View>
   );
 }
