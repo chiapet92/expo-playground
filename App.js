9f7a2f91471deb7aac1a8e7c0eeb4f1b0f268c77
chiapet92 <markxqi@gmail.com>
2021-08-03 17:44:31 -0700

    building screens

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
 import React from "react";
 import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 // View -> UIView for iOS
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", // main
-        alignItems: "center", // secondary axis
-        alignContent: "center", // only applies with wrapping
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flexBasis: 100, // primary axis
-          // width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-    </View>
-  );
+  // return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
