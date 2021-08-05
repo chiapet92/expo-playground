dc100adfdd6e27e810329f20f0d5fe7bcf18b547
chiapet92 <markxqi@gmail.com>
2021-08-04 22:11:36 -0700

    implementing flatlist

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 // View -> UIView for iOS
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
