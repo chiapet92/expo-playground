8f24cc7588aa688c7dbf45bb0e1d17bbaaf08ecf
chiapet92 <markxqi@gmail.com>
2021-08-04 21:32:43 -0700

    improving view image screen

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 // View -> UIView for iOS
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
