8eec1584ec71d37a544f4f664d7983221fa25250
chiapet92 <markxqi@gmail.com>
2021-08-07 07:30:50 -0700

    build listings screen

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountsScreen from "./app/screens/AccountsScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 
 // View -> UIView for iOS
 export default function App() {
-  return <AccountsScreen />;
+  return <ListingScreen />;
 }
