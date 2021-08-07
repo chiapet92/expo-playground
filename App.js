ac96c26d47cc38222217b77a2ece21a63bfa3ae0
chiapet92 <markxqi@gmail.com>
2021-08-06 18:25:00 -0700

    building accounts screen

--- App.js
+++ App.js
@@ -3,16 +3,9 @@ import React from "react";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
+import AccountsScreen from "./app/screens/AccountsScreen";
 
 // View -> UIView for iOS
 export default function App() {
-  return (
-    <Screen>
-      <ListItem
-        title="My title"
-        subtitle="My subtitle"
-        ImageComponent={<Icon name="email" />}
-      />
-    </Screen>
-  );
+  return <AccountsScreen />;
 }
--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,14 +2,13 @@ import React from "react";
 import Constants from "expo-constants";
 import { SafeAreaView, StyleSheet } from "react-native";
 
-function Screen({ children }) {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+function Screen({ children, style }) {
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
-    // backgroundColor: "yellow",
     flex: 1,
   },
 });
