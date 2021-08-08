4992be309996d0c4fd115a95675f9d8a83a779c0
chiapet92 <markxqi@gmail.com>
2021-08-08 08:34:53 -0700

    refactoring components directory

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormikContext } from "formik";
-import AppTextInput from "./AppTextInput";
+import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
 function AppFormField({ name, ...otherProps }) {
@@ -8,15 +8,9 @@ function AppFormField({ name, ...otherProps }) {
   return (
     <>
       <AppTextInput
-        // autoCapitalize="none"
-        // autoCorrect={false}
-        // icon="email"
-        // keyboardType="email-address"
         onBlur={() => setFieldTouched(name)}
         onChangeText={handleChange(name)}
         {...otherProps}
-        // placeholder="Email"
-        // textContentType="emailAddress"
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
