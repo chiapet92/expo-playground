2b615275687912fda02e47779085ad76b3d0bb9f
chiapet92 <markxqi@gmail.com>
2021-08-11 22:35:12 -0700

    permissions

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
