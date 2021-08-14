a43ef5fac20c0a11451052f342d2d2b9880c0490
chiapet92 <markxqi@gmail.com>
2021-08-14 09:53:09 -0700

    basic hooks

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
-import * as Location from "expo-location";
+import useLocation from "../hooks/useLocation";
 
 import {
   Form,
@@ -80,20 +80,7 @@ const categories = [
 ];
 
 function ListingEditScreen() {
-  const [location, setLocation] = useState();
-
-  const getLocation = async () => {
-    const { granted } = await Location.requestPermissionsAsync();
-    if (!granted) return;
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getLastKnownPositionAsync();
-    setLocation({ latitude, longitude });
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
+  const location = useLocation();
 
   return (
     <Screen style={styles.container}>
